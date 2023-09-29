cc9433c65d0723151057f89c8da37cc07f713eb5
illubaby <aironb177@gmail.com>
2023-09-29 10:00:04 +0700

    Init for EX1

--- Timer Interrupt and LED Scanning/Core/Src/main.c
+++ Timer Interrupt and LED Scanning/Core/Src/main.c
@@ -19,7 +19,7 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
-
+// Comment for check
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 
